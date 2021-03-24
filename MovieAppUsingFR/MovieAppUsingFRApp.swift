c93c1db81a741e8d094e078949ff25cc604df151
Mohammad Azam <azamsharp@gmail.com>
2021-03-24 13:11:06 -0500

    Initial Commit

---
+++ MovieAppUsingFR/MovieAppUsingFRApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppUsingFRApp.swift
+//  MovieAppUsingFR
+//
+//  Created by Mohammad Azam on 3/24/21.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppUsingFRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
